d10ac982b2e497761abecccc639f1e8f9a3add01
FerCod <fer__co@hotmail.com>
2022-08-23 15:03:15 -0300

    Login JWT

---
+++ BackEnd/src/main/java/com/portfolio/ferco/Security/Service/RolService.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.ferco.Security.Service;
+
+import com.portfolio.ferco.Security.Entity.Rol;
+import com.portfolio.ferco.Security.Enums.RolNombre;
+import com.portfolio.ferco.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author FerCod
+ */
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository iRolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return iRolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        iRolRepository.save(rol);
+    }
+}
---
+++ BackEnd/src/main/java/com/portfolio/ferco/Security/Service/UsuarioService.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.ferco.Security.Service;
+
+import com.portfolio.ferco.Security.Entity.Usuario;
+import com.portfolio.ferco.Security.Repository.iUsuarioRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author FerCod
+ */
+@Service
+@Transactional
+public class UsuarioService {
+    @Autowired
+    iUsuarioRepository iUsuarioRepository;
+    
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return iUsuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existsByNombreUsuario(String nombreUsuario){
+        return iUsuarioRepository.existsByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existsByEmail(String email){
+        return iUsuarioRepository.existsByEmail(email);
+    }
+    
+    public void save(Usuario usuario){
+        iUsuarioRepository.save(usuario);
+    }
+}
